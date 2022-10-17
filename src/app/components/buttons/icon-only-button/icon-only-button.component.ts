91964bfed130ceea635a4a002a98b21961831fba
Fanter <fanni.szasz96@gmail.com>
2022-10-17 21:44:07 +0200

    add icon-button

--- src/app/components/buttons/icon-only-button/icon-only-button.component.ts
+++ src/app/components/buttons/icon-only-button/icon-only-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-icon-only-button',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./icon-only-button.component.scss']
 })
 export class IconOnlyButtonComponent implements OnInit {
+  @Input() isDisabled: boolean = false;
 
   constructor() { }
 
