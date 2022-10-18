4b09485e9033d2522ea6af5217e5650cd69c69d5
Fanter <fanni.szasz96@gmail.com>
2022-10-18 15:02:07 +0200

    create spinner without disabled and, focus

--- src/app/components/menu-items/spinner/spinner.component.ts
+++ src/app/components/menu-items/spinner/spinner.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
-  styleUrls: ['./spinner.component.scss']
+  styleUrls: ['./spinner.component.scss'],
 })
 export class SpinnerComponent implements OnInit {
+  number:number = 0;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  increase() {
+    this.number++;
   }
 
+  decrease() {
+    if (this.number > 0) {
+      this.number--;
+    }
+  }
 }
