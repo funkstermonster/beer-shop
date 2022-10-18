45fd79f2946ec17c39fd53b06a0b0c4de5335fba
Fanter <fanni.szasz96@gmail.com>
2022-10-18 15:48:47 +0200

    build paging html

--- src/app/components/menu-items/paging/paging.component.ts
+++ src/app/components/menu-items/paging/paging.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-paging',
   templateUrl: './paging.component.html',
   styleUrls: ['./paging.component.scss']
 })
-export class PagingComponent implements OnInit {
+export class PagingComponent {
+
+  page: number = 1;
 
   constructor() { }
 
-  ngOnInit(): void {
+  nextPage() {
+    if (this.page < 5) {
+      this.page++
+    }
   }
 
+  prevPage() {
+    if (this.page > 1) {
+      this.page--
+    }
+  }
 }
